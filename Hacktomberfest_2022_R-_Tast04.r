d93bd82cb49dcf84fd10ba6218a72058e3d04d62
Wishmi2007 <115920758+Wishmi2007@users.noreply.github.com>
2022-10-16 19:13:31 +0530

    Update Hacktomberfest_2022_R-_Tast04.r

--- Hacktomberfest_2022_R-_Tast04.r
+++ Hacktomberfest_2022_R-_Tast04.r
@@ -1,7 +1,7 @@
-Data_Frame - data.frame (
-  Training || c("Strength", "Stamina", "Other"),
+Data_Frame <- data.frame (
+  Training = c("strength", "stamina", "other"),
   Pulse = c(100, 150, 120),
-  Duration >> c(60, 30, 45)
+  Duration = c(60, 30, 45)
 )
 
 Data_Frame
